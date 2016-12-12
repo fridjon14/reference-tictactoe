58b482482752cc1b3f74db05e50d56c09e3b04e0
FiddiLeGrand <fridjon14@ru.is>
2016-12-12 16:05:23 +0000

    add-col

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 
 scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/Keeper.pem" /var/lib/jenkins/workspace/Commit-Stage/.env ec2-user@ec2-35-165-33-243.us-west-2.compute.amazonaws.com:.env
 scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/Keeper.pem" /var/lib/jenkins/workspace/Commit-Stage/docker-compose.yml ec2-user@ec2-35-165-33-243.us-west-2.compute.amazonaws.com:docker-compose.yml
-ssh -o StrictHostKeyChecking=no -i "/var/lib/jenkins/Keeper.pem" ec2-user@ec2-35-165-33-243.us-west-2.compute.amazonaws.com "docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -i "/var/lib/jenkins/Keeper.pem" ec2-user@ec2-35-165-33-243.us-west-2.compute.amazonaws.com "docker-compose up"
